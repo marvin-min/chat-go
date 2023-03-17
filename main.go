b25afec9e56351d3870cec01657c838e2279cdac
marvin.min <marvin.min@activenetwork.com>
2023-03-17 09:27:37 +0800

    add timer

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	server := NewServer("0.0.0.0", 9999)
+	// go alert()
 	server.Start()
+}
+
+func alert() {
+	for {
+		time.Sleep(3 * time.Second)
+		fmt.Println("你该休息了")
+	}
 }
