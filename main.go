205bef712fd1f06b8a6114695ca7961243dc5fd6
marvin.min <marvin.min@activenetwork.com>
2023-03-22 15:15:26 +0800

    a clock

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 15:04:05")
+	clock.SetText(formatted)
+}
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
-
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
+	w := a.NewWindow("Clock")
+	clock := widget.NewLabel("")
+	updateTime(clock)
+	w.SetContent(clock)
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
 	w.ShowAndRun()
+	tidyUp()
+}
+
+func tidyUp() {
+	fmt.Println("Exited")
 }
