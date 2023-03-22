4982adc89278207f35841e0bb5c9504543d1f7e0
marvin.min <marvin.min@activenetwork.com>
2023-03-22 15:18:54 +0800

    file size

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/marvin-min/chat-go
 
 go 1.19
 
-require fyne.io/fyne/v2 v2.3.2
+require (
+	fyne.io/fyne v1.4.3
+	fyne.io/fyne/v2 v2.3.2
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20230312215936-7f71b037e260 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
@@ -16,6 +17,7 @@ func updateTime(clock *widget.Label) {
 func main() {
 	a := app.New()
 	w := a.NewWindow("Clock")
+	w.Resize(fyne.NewSize(200, 200))
 	clock := widget.NewLabel("")
 	updateTime(clock)
 	w.SetContent(clock)
@@ -24,7 +26,12 @@ func main() {
 			updateTime(clock)
 		}
 	}()
-	w.ShowAndRun()
+	w.Show()
+	w2 := a.NewWindow("Larger")
+	w2.SetContent(widget.NewLabel("More content"))
+	w2.Resize(fyne.NewSize(100, 100))
+	w2.Show()
+	a.Run()
 	tidyUp()
 }
 
